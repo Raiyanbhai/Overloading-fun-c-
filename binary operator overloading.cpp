8fda3bda7fb70b051f1288c28e5993fdcdf2ea9e
Raiyanbhai <101969805+Raiyanbhai@users.noreply.github.com>
2022-03-24 19:51:04 +0545

    Add files via upload

---
+++ binary operator overloading.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+class Distance
+{
+	private:float inch,feet;
+	public:Distance():inch(0),feet(0){}
+	       Distance(float i,float f):inch(i),feet(f){}
+	       void show()
+	       {cout<<"your distance is "<<inch<<"and"<<feet<<endl;
+	       	}
+	friend Distance operator +(Distance ,Distance);
+};
+ Distance operator +(Distance d,Distance d1)
+{
+	Distance temp;
+	temp.inch=d.inch+d1.inch;
+	temp.feet=d.feet+d1.feet;
+	return temp;
+}
+int main()
+{
+	Distance d(4,5);
+	Distance d2, d1(2,3);
+	d2=d+d1;
+	d2.show();
+	return 0;
+}
