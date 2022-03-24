24e735e8fdbc56eb99da56643237a755ade71970
Raiyanbhai <101969805+Raiyanbhai@users.noreply.github.com>
2022-03-24 20:00:48 +0545

    Add files via upload

---
+++ ovrlding concatenates string .cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+class Str
+{
+private:
+       char a[20];
+public:
+       Str()
+       {
+       strcpy(a," ");
+       }
+       Str(char aa[])
+       {
+       strcpy(a,aa);
+       }
+       void getdata()
+       {
+       cout<<"Enter your fname:";
+       cin>>a;
+       cout<<"Enter your lname:";
+       cin>>a;
+       }
+       void show()
+       {
+       cout<<"your concatenates name is: "<<a;
+       }
+       friend Str operator+(Str a1,Str a2);
+};
+Str operator+(Str a1,Str a2 )
+{
+Str temp;
+strcpy(temp.a,a1.a);
+strcat(temp.a,a2.a);
+return temp;
+}
+int main()
+{
+Str a1,a2,a3;//a1("Raiyan"),a2("khan"),a3;
+a1.getdata();
+a3=a1+a1;
+a3.show();
+return 0;
+}  
