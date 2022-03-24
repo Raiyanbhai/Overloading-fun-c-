702e9bef075f96dde9b8caa2c2567377dfe300a4
Raiyanbhai <101969805+Raiyanbhai@users.noreply.github.com>
2022-03-24 20:00:24 +0545

    Add files via upload

---
+++ Ovr lding fn compair two obj .cpp
@@ -0,0 +1,40 @@
+﻿// program to overload of using this operator(<) and compare with two object 
+#include<iostream>
+using namespace std;
+class Distance
+{
+	private:
+	        float feet,inches;
+	public:
+	       Distance()
+	       {
+	       	feet=6;
+	       	inches=7;
+	       	}
+	       	Distance(float f,float i)
+	       	{
+	       	feet=f;
+	        inches=i;
+	       }
+	       void shsowdata()
+	       {
+	       	cout<<feet<<"'"<<inches<<"\""<<endl;
+	       }
+friend bool operator<(Distance d1,Distance d2);
+};
+bool operator<(Distance d1,Distance d2)
+{
+	float value1=d1.feet*12+d1.inches;
+	float value2=d2.feet*12+d2.inches;
+	if(value1 < value2)
+	return true;
+	else
+	return false;
+}
+int main()
+{ 
+	Distance s1(8,8);
+	Distance s2(6,8);
+	cout<< (s1<s2) <<endl;
+	return 0;
+}
