5f41912fc25acb4793c5bef8c3cce6aef818a29e
Raiyanbhai <101969805+Raiyanbhai@users.noreply.github.com>
2022-03-24 19:55:46 +0545

    Add files via upload

---
+++ Over load( + = )operator.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+class Distance
+{
+	private:float inch,feet;
+	public:Distance()
+	      {
+	      inch=0;
+	      feet=0;
+	      }
+	      Distance(float i,float f)
+	      {
+	      inch=i;
+	      feet=f;
+	      }
+	      void show()
+	      {
+	      cout<<"your distance is:"<<inch<<"'"<<feet<<"\''";
+	      }
+	      void operator +=(Distance d1)
+	      {
+	      inch +=d1.inch;
+	      feet +=d1.feet;
+	      }
+};
+int main()
+{
+Distance d1(4,5),d2(3,4);
+d1 +=d2;
+d1.show();
+return 0;
+}
