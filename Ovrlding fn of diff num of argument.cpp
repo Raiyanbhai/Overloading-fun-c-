bdce199cd67ef6b671cfab5281d5ff917762c232
Raiyanbhai <101969805+Raiyanbhai@users.noreply.github.com>
2022-03-24 20:02:28 +0545

    Add files via upload

---
+++ Ovrlding fn of diff num of argument.cpp
@@ -0,0 +1,29 @@
+﻿//Function overloading program of different number of argument.
+
+#include <iostream>
+using namespace std;
+float avg(float x,float y)
+{
+	return(x+y);
+}
+float avg(float x,float y,float z)
+{
+	return (x+y+z);
+}
+int main()
+{
+	int a,b,c;
+	cout<<"Enter any first number : ";
+	cin>>a;
+	cout<<"Enter any second number : ";
+	cin>>b;
+	cout<<"Enter any third number : ";
+	cin>>c;
+	cout<<endl;
+	cout<<endl;
+	cout<<"(\"";
+	cout<<"Enter any first  two numberof add is :["<<avg(a,b)<<"]"<<endl;
+	cout<<"Enter any second three numberof add is :["<<avg(a,b,c)<<"]";
+	cout<<"\")";
+	return 0;
+}
