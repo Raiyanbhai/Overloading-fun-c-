f6ba63bfdda3a36c367ad538608728ae55f13703
Raiyanbhai <101969805+Raiyanbhai@users.noreply.github.com>
2022-03-24 20:03:38 +0545

    Add files via upload

---
+++ Ovrlding fn of diff type of argument.cpp
@@ -0,0 +1,23 @@
+﻿
+//Function overloading program of different Types of argument.
+
+#include <iostream>
+using namespace std;
+float add(float x,int y)
+{
+	return(x+y);
+}
+string add(string x,string y)
+{
+		return(x+y);
+}
+int main()
+{
+	int a=3,b=6;
+    cout<<add(a,b)<<endl;
+	string c="Raiyan",d="khan";
+	cout<<add(c,d)<<endl;
+	
+
+	return 0;
+}
